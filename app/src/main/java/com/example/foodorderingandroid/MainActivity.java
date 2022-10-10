02e011321b7393cc816ee600d0d61a32c1a1e5af
Alvlay <alvlaykevin@gmail.com>
2022-10-10 23:22:55 +0800

    2022/10/10 -3 add start page toast

--- app/src/main/java/com/example/foodorderingandroid/MainActivity.java
+++ app/src/main/java/com/example/foodorderingandroid/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.foodorderingandroid;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        //点击button_start按钮暂时弹出显示“正在进入系统”
+        Button button_start=findViewById(R.id.Button_start);
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"正在加载菜单……",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
